d4d8a4c12d976bed82a0689e0a23ecadb46e1a8d
VaricelaZoster <maildwaipayansaha@gmail.com>
2025-05-20 01:23:21 +0530

    bit more styling on Inbox info

--- intercom/src/assets/InboxInfo.jsx
+++ intercom/src/assets/InboxInfo.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const InboxInfo = () => {
   return (
-    <div className='flex justify-between'>
-        <div>5 open</div>
+    <div className='flex justify-between m-2 font-bold '>
+        <div className='relative inline-block text-left transition duration-150 ease-in hover:bg-gray-300 rounded-full'>
+            <button className='px-2 py-1'>5 open</button>
+        </div>
         <div> Waiting longest</div>
     </div>
   )
