b7b34df9eace484c7e549aa00b8d817646177403
VaricelaZoster <maildwaipayansaha@gmail.com>
2025-05-20 10:34:58 +0530

    drop down menu

--- intercom/src/assets/InboxInfo.jsx
+++ intercom/src/assets/InboxInfo.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
 
 const InboxInfo = () => {
+
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
-    <div className='flex justify-between m-2 font-bold '>
-        <div className='relative inline-block text-left transition duration-150 ease-in hover:bg-gray-300 rounded-full'>
-            <button className='px-2 py-1'>5 open</button>
+    <div className='flex justify-between m-2 font-bold gap-1'>
+      <Menu as="div" className="relative inline-block text-left">
+        <div>
+          <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-xs ring-1 ring-gray-300 ring-inset hover:bg-gray-50">
+            5 open
+          <ChevronDownIcon aria-hidden="true" className="-mr-1 size-5 text-gray-400" />
+          </MenuButton>
         </div>
+        <MenuItems
+        transition
+        className="flex flex-col right-0 z-10 mt-2 w-20 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
+      >
+        <div className="py-1">
+          <MenuItem>
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:text-gray-900 data-focus:outline-hidden"
+            >
+              Edit
+            </a>
+          </MenuItem>
+        </div>
+      </MenuItems>
+      </Menu>
         <div> Waiting longest</div>
     </div>
   )
